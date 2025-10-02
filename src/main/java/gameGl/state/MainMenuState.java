b55e842254f003406e3985321c16e9b2f0280921
Flamaiche <Flamaiche@gmail.com>
2025-10-02 14:02:20 +0200

    [A1.0] selection souris menu principal

--- src/main/java/gameGl/state/MainMenuState.java
+++ src/main/java/gameGl/state/MainMenuState.java
@@ -37,31 +37,50 @@ public class MainMenuState extends GameState {
         super.init(commande, width, height);
         initTouches();
         initHud();
+        initMouseCallbacks();
+    }
+
+    private void initMouseCallbacks() {
+        // Callback pour suivre la souris et mettre à jour la sélection
+        glfwSetCursorPosCallback(commande.getWindow(), (window, xpos, ypos) -> {
+            for (int i = 0; i < texts.size(); i++) {
+                TextHUD t = texts.get(i);
+                if (TextHUD.isMouseOver(t, (float)xpos, (float)ypos)) {
+                    indexSelection = i; // priorité souris
+                    break;
+                }
+            }
+        });
     }
 
     @Override
     public void initTouches() {
         ArrayList<Touche> touches = new ArrayList<>();
 
-        // Navigation menu
+        // Navigation menu clavier
         touches.add(new Touche(GLFW_KEY_UP, () -> indexSelection--, null, null));
         touches.add(new Touche(GLFW_KEY_DOWN, () -> indexSelection++, null, null));
         touches.add(new Touche(GLFW_KEY_ENTER, () -> actionBySelection(indexSelection), null, null));
         touches.add(new Touche(GLFW_KEY_ESCAPE, () -> actionBySelection(2), null, null));
 
-        commande.setTouches(touches);
+        // Clic souris
+        touches.add(new Touche(GLFW_MOUSE_BUTTON_LEFT, true, () -> actionBySelection(indexSelection), null, null));
 
+        commande.setTouches(touches);
     }
 
     @Override
     public void initHud() {
-        for (String t : textMenu)
-            texts.add(new TextHUD(null, t, TextHUD.HorizontalAlignment.CENTER, TextHUD.VerticalAlignment.CENTER, (float)(uniformTextScale*1.2), 1.0f, 1.0f, 1.0f));
+        // Textes menu
+        for (String t : textMenu) {
+            texts.add(new TextHUD(null, t, TextHUD.HorizontalAlignment.CENTER, TextHUD.VerticalAlignment.CENTER,
+                    (float)(uniformTextScale*1.2), 1.0f, 1.0f, 1.0f));
+        }
         hud.setTexts(texts);
 
         // HUD animé
-        for (int i=0; i<nbStarCircle1; i++) {
-            textsVollatile.add( new TextHUD(null, "*", null, null, uniformTextScale, 1.0f, 0f, 0f));
+        for (int i = 0; i < nbStarCircle1; i++) {
+            textsVollatile.add(new TextHUD(null, "*", null, null, uniformTextScale, 1.0f, 0f, 0f));
         }
         textManagerVolatille.setTexts(textsVollatile);
     }
@@ -70,7 +89,7 @@ public class MainMenuState extends GameState {
         switch (indexSelection) {
             case 0 -> commande.getGameStateManager().setState(GameStateManager.GameStateEnum.NEWPLAY);
             case 1 -> System.out.println("Paramètre");
-            case 2 ->  glfwSetWindowShouldClose(commande.getWindow(), true);
+            case 2 -> glfwSetWindowShouldClose(commande.getWindow(), true);
         }
     }
 
@@ -95,10 +114,10 @@ public class MainMenuState extends GameState {
         }
 
         // Animation du HUD tournant
-        totalTime += deltaTime/2;
-        int degres = 360/textsVollatile.size();
-        for (int i=0; i<textsVollatile.size(); i++) {
-            circularText(textsVollatile.get(i), totalTime+((float) (degres * (i + 1)) /360));
+        totalTime += deltaTime / 2;
+        int degres = 360 / textsVollatile.size();
+        for (int i = 0; i < textsVollatile.size(); i++) {
+            circularText(textsVollatile.get(i), totalTime + ((float) (degres * (i + 1)) / 360));
         }
     }
 
@@ -107,8 +126,6 @@ public class MainMenuState extends GameState {
         rotatingText.setX((float) pos[0]);
         rotatingText.setY((float) pos[1]);
     }
-
-
 
     @Override
     public void update(float deltaTime) {
