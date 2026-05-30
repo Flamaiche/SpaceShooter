f3788f0864f9c630725240d3ccab304d9b5fd778
hrichard <hrichard@users.noreply.github.com>
2026-05-30 19:17:57 +0200

    [A1.1] corrige: balles spawn à joueur.getPosition()

--- src/main/java/gamegl/state/PlayingState.java
+++ src/main/java/gamegl/state/PlayingState.java
@@ -294,9 +294,7 @@ public class PlayingState extends GameState {
         Vector3f rayDir = crosshair.getRayDir();
 
         ConfigVaisseau vaisseau = ConfigVaisseau.get();
-        Vector3f spawnPos = new Vector3f(joueur.getPosition())
-                .add(new Vector3f(rayDir).mul(vaisseau.bulletOffset.x))
-                .sub(new Vector3f(camera.getUp()).mul(vaisseau.bulletOffset.y));
+        Vector3f spawnPos = new Vector3f(joueur.getPosition());
 
         for (Balls b : balls) {
             if (!b.isActive()) {
